e2536cdf8bc710a092239c42666863c16ff6f667
PabloProta <antoniniprota@gmail.com>
2021-02-12 09:52:51 -0300

    add compat version API -30

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -9,6 +9,9 @@ import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+   lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,7 +23,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun roll_Dice() {
-        val diceImage: ImageView = findViewById(R.id.dice_image)
+        diceImage = findViewById(R.id.dice_image)
         val drawableResource = when ((1..6).random()){
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
